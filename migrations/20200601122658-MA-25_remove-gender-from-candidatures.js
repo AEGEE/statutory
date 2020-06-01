2b781f31298d6fe819c5210d87bbb50ed0531f38
Flo <HassanCehef@users.noreply.github.com>
2020-06-01 14:59:24 +0200

    fix: linter

--- migrations/20200601122658-MA-25_remove-gender-from-candidatures.js
+++ migrations/20200601122658-MA-25_remove-gender-from-candidatures.js
@@ -1,12 +1,10 @@
-'use strict';
-
 module.exports = {
+    up: queryInterface => queryInterface.removeColumn('candidates', 'gender'),
     down: (queryInterface, Sequelize) => queryInterface.addColumn(
         'candidates',
         'gender',
         {
             type: Sequelize.STRING, allowNull: false, defaultValue: '',
         }
-    ),
-    up: queryInterface => queryInterface.removeColumn('candidates', 'gender')
+    )
 };
